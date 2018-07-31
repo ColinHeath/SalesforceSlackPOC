1f8bbe25da1b3069a3c8e88c67d14598d1220039
Colin Heath <colinjhe@usc.edu>
2018-07-30 21:44:34 -0400

    Refactored app.js to make addition of new functionality easier. Can now create an account easily, with no duplicates

--- NodeTest/app.js
+++ NodeTest/app.js
@@ -1,3 +1,34 @@
+function loginToSalesforce(sfConnection, methodOnLogin, paramObject)
+{
+    //Add config readin for user credentials.
+    sfConnection.login((err, userInfo) => {
+        if(err) { return console.error(err); }
+        console.log(sfConnection.accessToken);
+        console.log(sfConnection.instanceUrl);
+
+        methodOnLogin(sfConnection, paramObject);
+    })
+}
+
+function createAccount(sfConnection, accountObject)
+{
+    //Need to query salesforce, make sure no account with this name already exists.
+    sfConnection.query("SELECT Id, Name FROM Account", (err, result) => {
+        if(err) { return console.error(err) };
+
+        for(let i = 0; i < result.records.length; i++)
+        {
+            if((result.records[i])["Name"] === accountObject["Name"])
+            {
+                console.log("entered false return");
+                return;
+            }
+        }
+
+        sfConnection.create("Account", accountObject);
+    });
+}
+
 let http = require('http');
 let url = require('url');
 let jsforce = require('jsforce');
@@ -5,38 +36,21 @@ let jsforce = require('jsforce');
 http.createServer((req, res) => {
     if(req.url != '/favicon.ico')
     {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-    
-        let queryValues = url.parse(req.url, true).query;
+        res.writeHead(200, {'Content-Type': 'application/json'});
     
         let conn = new jsforce.Connection({
             loginUrl : 'https://test.salesforce.com'
         });
         
-        //DO NOT LEAVE PLAINTEXT CREDENTIALS IN
-        conn.login(/*user*/, /*pass*/, (err, userInfo) => {
-            if(err) { return console.error(err); }
-            console.log(conn.accessToken);
-            console.log(conn.instanceUrl);
-    
-            console.log("User ID: " + userInfo.id);
-            console.log("Org ID: " + userInfo.organizationId);
-        
-            conn.query("SELECT Id, Name FROM Account", (err, result) => {
-                if(err) { return console.error(err) };
-                console.log("total : " + result.totalSize);
-                console.log("fetched : " + result.records.length);
-                console.log(result.records[0]);
+        let newAccount = {
+            attributes: {type: 'Account'},
+            Name: 'Colin\'s Cool Corporation'
+        };
 
-            });
+        res.write(JSON.stringify(newAccount));
 
-            conn.create("Account", { 
-                attributes: { type: 'Account' },
-                Name: 'Ten-Ten' 
-            });
-        });
+        loginToSalesforce(conn, createAccount, newAccount);
     
         res.end();
     }
 }).listen(8080);
-
