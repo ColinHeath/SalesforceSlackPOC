fd4f74d3efd9c3faef0425feeb380227c86c9aac
Colin Heath <colinjhe@usc.edu>
2018-08-02 16:45:19 -0400

    Introduced clean lead creation and conversion. Conversion does not work with Slack yet.

--- NodeTest/app.js
+++ NodeTest/app.js
@@ -20,27 +20,79 @@ function createAccount(sfConnection, parameterObject)
     };
 
     //Need to query salesforce, make sure no account with this name already exists.
-    sfConnection.query("SELECT Name FROM Account", (err, result) => {
-        if(err) { return console.error(err) };
+    sfConnection.sobject("Account")
+        .select('*')
+        .execute((err, records) => {
+            if(err) { return console.error(err) };
 
-        let allowCreation = true;
+            let creationAllowed = checkForDuplicateObjects(records, newAccount);
 
-        for(let i = 0; i < result.records.length; i++)
+            console.log("Allowed: " + creationAllowed);
+
+            if(creationAllowed) sfConnection.create("Account", newAccount);
+
+            console.log("After creation or not");
+
+            return;
+        });
+
+    console.log("Leaving Account Creation");
+    return;
+}
+
+function checkForDuplicateObjects(objectRecords, objectToCheck)
+{
+    let allowCreation = true;
+
+    for(let i = 0; i < objectRecords.length; i++)
+    {
+        let areIdentical = true;
+
+        for(let property in objectToCheck)
         {
-            if((result.records[i])["Name"] === newAccount["Name"])
+            if(objectRecords[i].hasOwnProperty(property) && objectToCheck.hasOwnProperty(property))
             {
-                allowCreation = false;
-                break;
+                if(property != "attributes")
+                {
+                    console.log(property);
+                    console.log(objectRecords[i][property]);
+                    console.log(objectToCheck[property]);
+
+                    if(objectRecords[i][property] != null && objectToCheck[property] != null)
+                    {
+                        areIdentical = areIdentical && (objectRecords[i][property] == objectToCheck[property]);
+                    }
+
+                    console.log("Identical: " + areIdentical);
+                }
             }
         }
 
-        if(allowCreation) sfConnection.create("Account", newAccount);
-    });
+        if(areIdentical)
+        {
+            allowCreation = false;
+            break;
+        }
+    }
+
+    return (allowCreation);
 }
 
 function switchSalesforceAction(sfConnection, inputData)
 {
-    if(inputData.hasOwnProperty("accountData"))
+    if(inputData == null)
+    {
+        return;
+    }
+    else if(inputData.hasOwnProperty("convertData"))
+    {
+        console.log("Getting Ready to Convert Lead");
+        let convertData = JSON.parse(inputData["convertData"]);
+        convertData = convertData[0];
+
+        loginToSalesforce(sfConnection, convertLead, convertData);
+    }
+    else if(inputData.hasOwnProperty("accountData"))
     {
         console.log("Getting Ready to Make Account");
         let accountData = JSON.parse(inputData["accountData"]);
@@ -58,6 +110,91 @@ function switchSalesforceAction(sfConnection, inputData)
     }
 }
 
+function convertLead(sfConnection, parameterObject)
+{
+    /*
+        -Account Name
+        -Record Owner
+        -Converted Status
+    */
+
+    /*
+        -convertedStatus
+        -leadId --> end required
+        -accountId
+        -contactId
+        -doNotCreateOpportunity
+        -opportunityName
+        -overwriteLeadSource
+        -ownerId
+        -sendNotificationEmail
+    */
+    console.log("In Function");
+    sfConnection.sobject("Lead")
+        .select('Name, Company, Id')
+        .execute((err, records) => {
+            if(err) { console.error(err) };
+
+            console.log("Behind Select");
+            let leadObjectToConvert = null;
+
+            for(let i = 0; i < records.length; i++)
+            {
+                console.log("Lead Name: " + records[i]["Name"]);
+                if(records[i]["Name"] == parameterObject["Lead Name"])
+                {
+                    leadObjectToConvert = records[i];
+                    break;
+                }
+            }
+
+            if(leadObjectToConvert != null)
+            {
+                let accountObject = {
+                    "Account Name": leadObjectToConvert["Company"]
+                }
+
+                createAccount(sfConnection, accountObject);
+                let convertedAccountId = null;
+
+                sfConnection.sobject("Account")
+                    .select("Id, Name")
+                    .execute((err, accountRecords) => {
+                        if(err) { return console.error(err) };
+
+                        for(let i = 0; i < accountRecords.length; i++)
+                        {
+                            if(accountRecords[i]["Name"] == accountObject["Account Name"])
+                            {
+                                convertedAccountId = accountRecords[i]["Id"];
+                            }
+                        }
+
+                        if(accountId)
+                        {
+                            let leadConvert = {
+                                convertedStatus: parameterObject["Converted Status"],
+                                leadId: leadObjectToConvert["Id"],
+                                opportunityName: parameterObject["Opportunity Name"],
+                                doNotCreateOpportunity: false,
+                                accountId: convertedAccountId
+                            }
+
+                            sfConnection.soap.convertLead(leadConvert, (err, result) => {
+                                if(err) { return console.error(err) };
+                                console.log("Success? " + result.success);
+                                console.log("New Opportunity ID: " + result.opportunityId);
+                            })
+                        }
+                    })
+            }
+        });
+
+    /*    let soap = new SoapApi(sfConnection);
+
+    soap.convertLead()*/
+}
+
 function createLead(sfConnection, parameterObject)
 {
     let newLead = {
@@ -70,27 +207,22 @@ function createLead(sfConnection, parameterObject)
         Status: parameterObject["Lead Status"]
     }
 
-    sfConnection.query("SELECT LastName FROM Lead", (err, result) => {
-        if(err) { return console.error(err) };
+    sfConnection.sobject("Lead")
+        .select('*')
+        .execute((err, records) => {
+            if(err) { return console.error(err) };
 
-        for(let i = 0; i < result.records.length; i++)
-        {
-            //TODO: Fix this filter
-            if((result.records[i])["LastName"] === newLead["LastName"])
-            {
-                console.log("Lead Would have been duplicate");
-                return;
-            }
-        }
+            let creationAllowed = checkForDuplicateObjects(records, newLead);
 
-        sfConnection.create("Lead", newLead);
-    });
+            console.log("Allowed: " + creationAllowed);
+
+            if(creationAllowed) sfConnection.create("Lead", newLead);
+        })
 }
 
 function initializeServer()
 {
     let http = require('http');
-    let url = require('url');
     let jsforce = require('jsforce');
 
     let conn = new jsforce.Connection({
@@ -117,9 +249,12 @@ function initializeServer()
                     console.log(inputData);
 
                     switchSalesforceAction(conn, inputData);
-                })    
+                })
             }
-    
+            else
+            {
+                switchSalesforceAction(conn, null);
+            }
             res.end();
         }
     }).listen(8080);
